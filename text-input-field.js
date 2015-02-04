59fd783363ca1fd9ad1a6f0ae8ec98e664e1be99
Thomas Marstrander <thomasmars87@gmail.com>
2015-02-04 14:16:49 +0100

    Created library text input field

---
+++ text-input-field.js
@@ -0,0 +1,53 @@
+var H5P = H5P || {};
+
+/**
+ * Text Input Field module
+ * @external {jQuery} $ H5P.jQuery
+ */
+H5P.TextInputField = (function ($) {
+  // CSS Classes:
+  var MAIN_CONTAINER = 'h5p-text-input-field';
+  var INPUT_LABEL = 'h5p-text-input-field-label';
+  var INPUT_FIELD = 'h5p-text-input-field-textfield';
+
+  /**
+   * Initialize module.
+   * @param {Object} params Behavior settings
+   * @param {Number} id Content identification
+   * @returns {Object} TextInputField TextInputField instance
+   */
+  function TextInputField(params, id) {
+    this.$ = $(this);
+    this.id = id;
+
+    // Set default behavior.
+    this.params = $.extend({}, {
+      taskDescription: 'Input field',
+      placeholderText: '',
+      inputFieldSize: '1'
+    }, params);
+  }
+
+  /**
+   * Attach function called by H5P framework to insert H5P content into page.
+   *
+   * @param {jQuery} $container The container which will be appended to.
+   */
+  TextInputField.prototype.attach = function ($container) {
+    var self = this;
+    this.$inner = $container.addClass(MAIN_CONTAINER);
+
+    $('<div>', {
+      'class': INPUT_LABEL,
+      'html': self.params.taskDescription
+    }).appendTo(self.$inner);
+
+    $('<textarea>', {
+      'class': INPUT_FIELD,
+      'rows': parseInt(self.params.inputFieldSize),
+      'placeholder': self.params.placeholderText
+    }).appendTo(self.$inner);
+  };
+
+  return TextInputField;
+})(H5P.jQuery);
